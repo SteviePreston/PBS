c92b32e61b6f691dd370c2800b9d973a8d1171b4
Philip Reed <philipreed@Patricks-iPod-touch.local>
2023-03-14 13:00:55 +0000

    phil branch: header admin sort of works

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { CalendarComponent } from './calendar/calendar.component';
-
+import { BookingComponent } from './booking/booking.component';
+import { AccountModificationComponent } from './account-modification/account-modification.component';
 
 const routes: Routes = [
-  { path: 'register', component: RegisterComponent },
+  { path: 'calendar', component: CalendarComponent},
+  { path: 'booking', component: BookingComponent},
+  { path: 'accountModification', component: AccountModificationComponent},
   { path: 'login', component: LoginComponent },
-  {path: 'calendar', component: CalendarComponent},
-
+  { path: 'register', component: RegisterComponent },
+  //TODO: Add sign out functionality
 ];
 
 @NgModule({
--- frontend/src/app/auth-user.service.ts
+++ frontend/src/app/auth-user.service.ts
@@ -1,11 +1,40 @@
 import { Injectable } from '@angular/core';
-
-
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router'
+import jwt_decode from 'jwt-decode';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthUserService {
-  constructor() { }
+  private _userIsAdmin = new BehaviorSubject<boolean>(false);
+  constructor(private http: HttpClient, private router: Router) { }
   
+  async checkAdmin() {
+    const token = localStorage.getItem('token') as string;
+    const decodedtoken  = jwt_decode(token) as any;
+    const email = decodedtoken.email;
+    const apiPath = 'http://localhost:3000/v1/api/admin/';
+
+    this.http.get(apiPath + email).subscribe(response => {
+      this._userIsAdmin.next(response == true);
+      console.log(this._userIsAdmin.value);
+
+     }, errors=>{
+        console.error(errors);
+        this.router.navigate(['/login']);
+        alert("Login has expired!, Please login again");
+     }
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+  
+  get userIsAdmin() {
+    return this._userIsAdmin.asObservable();
+  }
 }
