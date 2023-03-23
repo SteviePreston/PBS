d970c9eca32fce44a4eb6c27a52a1e32a4f6a54a
Philip Reed <philipreed@Patricks-iPod-touch.local>
2023-03-23 18:27:54 +0000

    unbreaking

--- frontend/src/app/booking/booking.component.ts
+++ frontend/src/app/booking/booking.component.ts
@@ -164,7 +164,7 @@ export class BookingComponent {
     const token = localStorage.getItem('token') as string;
     const decoded = jwt_decode(token) as any;
     const email = decoded.email;
-    //data.email = email
+    data.email = email
     //get customer id 
     //data.customerID = this.getCustomerID();
 
